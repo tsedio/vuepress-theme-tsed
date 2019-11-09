8257c94ea72ea24332fc11b51036115f0119edc5
romain.lenzotti <rom.lenzotti@gmail.com>
2019-11-09 13:43:31 +0100

    fix: Move Layout at the right place for vuepress v1

--- src/layouts/NotFound.vue
+++ src/layouts/NotFound.vue
@@ -46,7 +46,7 @@
 <script>
   import nprogress from 'nprogress'
   import Vue from 'vue'
-  import VueTsed, { resolveSidebarItems } from './'
+  import VueTsed, { resolveSidebarItems } from '../'
 
   Vue.use(VueTsed)
 
