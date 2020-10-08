b64ba05660cc4db73266aad1ee9d33d134a38944
Romain Lenzotti <rom.lenzotti@gmail.com>
2020-10-08 10:19:33 +0200

    chore: Add storybook deployment
    (cherry picked from commit de0b591d53ae7396ad0bd106f54c35376f018925)

--- release.config.js
+++ release.config.js
@@ -1,12 +1,17 @@
 module.exports = {
-  branch: 'master',
-  verifyConditions: ['@semantic-release/github', '@semantic-release/npm', '@tsed/monorepo-utils/semantic-release'],
-  analyzeCommits: ['@semantic-release/commit-analyzer'],
+  branches: [
+    "master",
+    {name: "alpha", prerelease: true, channel: "alpha"},
+    {name: "beta", prerelease: true, channel: "beta"},
+    {name: "rc", prerelease: true, channel: "rc"}
+  ],
+  verifyConditions: ["@semantic-release/github", "@semantic-release/npm", "@tsed/monorepo-utils/semantic-release"],
+  analyzeCommits: ["@semantic-release/commit-analyzer"],
   verifyRelease: [],
-  generateNotes: ['@semantic-release/release-notes-generator'],
-  prepare: ['@semantic-release/npm', '@tsed/monorepo-utils/semantic-release'],
-  publish: ['@tsed/monorepo-utils/semantic-release', '@semantic-release/github'],
-  success: ['@semantic-release/github', '@tsed/monorepo-utils/semantic-release'],
-  fail: ['@semantic-release/github'],
+  generateNotes: ["@semantic-release/release-notes-generator"],
+  prepare: ["@semantic-release/npm", "@tsed/monorepo-utils/semantic-release"],
+  publish: ["@tsed/monorepo-utils/semantic-release", "@semantic-release/github"],
+  success: ["@semantic-release/github", "@tsed/monorepo-utils/semantic-release"],
+  fail: ["@semantic-release/github"],
   npmPublish: false
-}
+};
