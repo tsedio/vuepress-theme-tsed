80de7dc7d182f18597efa22c97b5d5275ce092be
Romain Lenzotti <rom.lenzotti@gmail.com>
2020-11-05 15:41:54 +0100

    feat: Add support block

---
+++ packages/vuepress-common/src/components/organisms/support-us/SupportUsBlock.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <Showcase :class="backers.classes"
+              v-bind="backers"
+              v-if="backers">
+
+      <OpenCollectiveBackers v-bind="backers.badge"/>
+
+      <template #showcase-cta>
+        <Button
+            :bg-color="backers.cta.bgColor"
+            :color="backers.cta.color"
+            rounded="medium"
+            :href="backers.cta.url">
+          {{ backers.cta.label }}
+        </Button>
+      </template>
+    </Showcase>
+
+    <Showcase :class="sponsors.classes"
+              v-bind="sponsors">
+
+      <OpenCollectiveSponsors v-bind="sponsors.badge"/>
+
+      <div v-if="sponsors.partners" class="flex flex-wrap mb-8 -mx-3 sm:-mx-5">
+        <PartnerLogo
+            class="w-1/3 sm:w-1/5 p-3 sm:p-5 flex items-center justify-center"
+            v-for="partner in sponsors.partners"
+            :key="partner.href"
+            :src="partner.src"
+            :href="partner.href"
+            :title="partner.title"/>
+      </div>
+
+      <template #showcase-cta>
+        <Button
+            :bg-color="sponsors.cta.bgColor"
+            :color="sponsors.cta.color"
+            rounded="medium"
+            :href="sponsors.cta.url">
+          {{ sponsors.cta.label }}
+        </Button>
+      </template>
+    </Showcase>
+  </div>
+</template>
+<script>
+import { Button, Showcase } from '../../molecules'
+import { OpenCollectiveBackers, OpenCollectiveSponsors } from '../index'
+
+export default {
+  name: 'SupportUsBlock',
+  components: {
+    OpenCollectiveBackers,
+    OpenCollectiveSponsors,
+    Button,
+    Showcase
+  },
+  computed: {
+    backers () {
+      const { backers } = this.$page.frontmatter
+      return backers
+    },
+    sponsors () {
+      const { sponsors } = this.$page.frontmatter
+      return sponsors
+    }
+  }
+}
+</script>
--- packages/vuepress-theme-tsed/src/views/Home.vue
+++ packages/vuepress-theme-tsed/src/views/Home.vue
@@ -91,48 +91,7 @@
       </template>
     </Showcase>
 
-    <Showcase :class="backers.classes"
-              v-bind="backers"
-              v-if="backers">
-
-      <OpenCollectiveBackers v-bind="backers.badge"/>
-
-      <template #showcase-cta>
-        <Button
-            :bg-color="backers.cta.bgColor"
-            :color="backers.cta.color"
-            rounded="medium"
-            :href="backers.cta.url">
-          {{ backers.cta.label }}
-        </Button>
-      </template>
-    </Showcase>
-
-    <Showcase :class="sponsors.classes"
-              v-bind="sponsors">
-
-      <OpenCollectiveSponsors v-bind="sponsors.badge"/>
-
-      <div v-if="sponsors.partners" class="flex flex-wrap mb-8 -mx-3 sm:-mx-5">
-        <PartnerLogo
-            class="w-1/3 sm:w-1/5 p-3 sm:p-5 flex items-center justify-center"
-            v-for="partner in sponsors.partners"
-            :key="partner.href"
-            :src="partner.src"
-            :href="partner.href"
-            :title="partner.title"/>
-      </div>
-
-      <template #showcase-cta>
-        <Button
-            :bg-color="sponsors.cta.bgColor"
-            :color="sponsors.cta.color"
-            rounded="medium"
-            :href="sponsors.cta.url">
-          {{ sponsors.cta.label }}
-        </Button>
-      </template>
-    </Showcase>
+    <SupportUsBlock />
 
     <slot name="page-bottom"/>
   </div>
@@ -164,7 +123,6 @@ export default {
     Showcase
   },
   computed: {
-
     gettingStartedUrl () {
       const { gettingStartedUrl } = this.$page.frontmatter
       return gettingStartedUrl
@@ -188,14 +146,6 @@ export default {
     contributors () {
       const { contributors } = this.$page.frontmatter
       return contributors
-    },
-    backers () {
-      const { backers } = this.$page.frontmatter
-      return backers
-    },
-    sponsors () {
-      const { sponsors } = this.$page.frontmatter
-      return sponsors
     },
     liveDemoUrl () {
       const { liveDemoUrl } = this.$page.frontmatter
