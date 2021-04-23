35581bcb0f72b16cc427a68b91efc3bdb351548f
Romain Lenzotti <rom.lenzotti@gmail.com>
2021-04-23 09:28:05 +0200

    fix: Add more socials types

--- packages/vuepress-common/src/utils/socials/socials.js
+++ packages/vuepress-common/src/utils/socials/socials.js
@@ -1,6 +1,9 @@
 export const SOCIALS = [
   { type: 'github', title: 'Github', icon: 'bxl-github' },
   { type: 'gitter', title: 'Gitter', icon: 'bx-message-rounded-dots' },
+  { type: 'slack', title: 'Slack', icon: 'bxl-slack' },
+  { type: 'discord', title: 'Discord', icon: 'bxl-discord' },
+  { type: 'microsoftTeams', title: 'Microsoft Teams', icon: 'bxl-microsoft-teams' },
   { type: 'twitter', title: 'Twitter', icon: 'bxl-twitter' },
   { type: 'stackoverflow', title: 'StackOverflow', icon: 'bxl-stack-overflow' },
   { type: 'sponsor', title: 'Sponsor', icon: 'bxs-heart' }
